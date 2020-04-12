01da280257f4cc320cde29c3b171597a7f8e2286
Nikki <nikmck22@gmail.com>
2020-04-12 12:25:20 -0400

    add the index and update action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p>{{ sample }}</p>
-    <!-- <div v-bind:key="contact.id" v-for="contact in contacts"></div>
-    <p>{{ contact.first_name }}</p> -->
+    <p>First Name: <input type="text" v-model="newContactFirstName"></p>
+    <button v-on:click="addContact()">Add a new contact</button>    
+    <div v-bind:key="contact.id" v-for="contact in contacts">
+    <p>{{ contact.id }} - {{ contact.first_name }} {{ contact.last_name }}</p>
+    <p>{{ contact.email }}</p>
+    <p>{{ contact.phone }}</p>
+    </div>
   </div>
 </template>
 
@@ -17,19 +21,33 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      sample: "Here is this working",
-      contacts: []
+      sample: "Let's change some text",
+      contacts: [],
+      newContactFirstName: "",
+      newContactLastName: "",
+      newContactEmail: "",
+      newContactPhoneNumber: ""
 
     };
   },
   
   created: function() {
     axios.get("/api/contacts").then(response => {
-      console.log('response.data')
+      console.log('Lets show some data');
+      this.contacts = response.data;
     });
   },
   methods: {
-   
+    addContact: function() {
+      console.log('Here is the new contact');
+      var params = {
+        input_first: this.newContactFirstName,
+        input_last: this.newContactLastName,
+        input_email: this.newContactEmail,
+        input_phone_number: this.newContactPhoneNumber
+      };
+    }
   }
 };
 </script>
+
